9bac8d5ff2388dab4553a9716ac0c8cd043e2e4f
Matej Fonfer <xfonfer@fi.muni.cz>
2018-03-26 19:52:10 +0200

    3.3 player class

---
+++ SOSCSRPG/Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        String Name { get; set; }
+        String CharacterClass { get; set; }
+        int HitPoints { get; set; }
+        int ExperiencePoints { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
+    }
+}
